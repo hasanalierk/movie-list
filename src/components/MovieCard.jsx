4a6dacd20c3f12e50e8208042940d14c7e76e85e
hasanalierk <hasanalierk98@gmail.com>
2023-03-17 14:49:08 +0300

    semi finally

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
@@ -7,19 +8,24 @@ const defaultImage =
 
 const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
   const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  const getVoteClass = (vote) =>{
-    if(vote >= 8){
-      return "green"
-    }else if ( vote >= 6){
-      return "orange"
-    }else{
-      return "red"
+  const getVoteClass = (vote) => {
+    if (vote >= 8) {
+      return "green";
+    } else if (vote >= 6) {
+      return "orange";
+    } else {
+      return "red";
     }
-  }
+  };
 
   return (
-    <div className="movie" id="container">
+    <div
+      className="movie"
+      id="container"
+      onClick={() => navigate("/details/" + id)}
+    >
       <img
         loading="lazy"
         src={poster_path ? IMG_API + poster_path : defaultImage}
@@ -27,7 +33,12 @@ const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
       />
       <div className="flex align-baseline justify-between p-1 text-white">
         <h5>{title}</h5>
-        {currentUser && <span className={`tag ${getVoteClass(vote_average)}  `} > {vote_average.toFixed(1)} </span>}
+        {currentUser && (
+          <span className={`tag ${getVoteClass(vote_average)}  `}>
+            {" "}
+            {vote_average.toFixed(1)}{" "}
+          </span>
+        )}
       </div>
       <div className="movie-over">
         <h2>Overview</h2>
